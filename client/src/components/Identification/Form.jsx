964ae6f5cfda148ddc8c2384db903995bcfcc8c3
mrpe4enushkaa_ <daniillavrika1980@gmail.com>
2025-03-19 23:39:40 +0300

    delete styles from form

--- client/src/components/Identification/Form.jsx
+++ client/src/components/Identification/Form.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 export default function Form({ data }) {
     return (
-        <form className="block--center" id="form-indefication">
+        <form className="block--center font-regular" id="form-indefication">
             <span className="heading font-medium">{data.name}</span>
 
             {data.email === true &&
                 (<>
-                    <label htmlFor="email" className="font-regular">Email</label>
-                    <input id="email" type="text" className="font-regular" />
+                    <label htmlFor="email" >Email</label>
+                    <input id="email" type="text" />
                 </>)}
 
-            <label htmlFor="login" className="font-regular">Username</label>
-            <input id="login" type="text" className="font-regular" />
-            <label htmlFor="password" className="font-regular">Password</label>
-            <input id="password" type="text" className="font-regular" />
+            <label htmlFor="login" >Username</label>
+            <input id="login" type="text" />
+            <label htmlFor="password" >Password</label>
+            <input id="password" type="text" />
 
             {data.repeatPassword === true && (<>
-                <label htmlFor="password-repeat" className="font-regular">Repeat the password</label>
-                <input id="password-repeat" type="text" className="font-regular" />
+                <label htmlFor="password-repeat" >Repeat the password</label>
+                <input id="password-repeat" type="text" />
             </>)}
         </form>
     );
