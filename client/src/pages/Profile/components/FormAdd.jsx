e14dca5ed79207230e0bea0497161c25400f1280
mrpe4enushkaa_ <daniillavrika1980@gmail.com>
2025-05-09 20:57:19 +0300

    add design form add document(s) or password

--- client/src/pages/Profile/components/FormAdd.jsx
+++ client/src/pages/Profile/components/FormAdd.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import Cloud from "../../../images/Cloud.png?react";
+import CloudDone from "../../../images/CloudDone.png?react";
 
 export default function FormAdd({ setName, isPassword, setIsPassword, setFiles, addDocument, formAdd, setPassword }) {
+    const [countFiles, setCountFiles] = useState(0);
+
     return (
         <form className="profile__form close" onSubmit={addDocument} ref={formAdd}>
-            <div className='form__add-document'>
-                <input type="text" placeholder="name" onChange={(e) => setName(e.target.value)} />
-                <select id="" defaultValue="password" onChange={(e) => setIsPassword(e.target.value === "password")}>
-                    <option value="password" selected>Password</option>
-                    <option value="document">Document</option>
-                </select>
-                {isPassword ? (<>
-                    <input type="text" placeholder='password: *********' onChange={(e) => setPassword(e.target.value)} />
-                    <button type="submit">submit</button>
-                </>) : (<>
-                    <input type="file" multiple onChange={(e) => setFiles(Array.from(e.target.files))} />
-                    <button type="submit">submit</button>
-                </>)}
+            <div className='form__add'>
+                <div className="form__container-div">
+                    <span className="form__name-dot font-regular">Name</span>
+                    <input type="text" className="form__input" onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div className="form__container-div">
+                    <span className="form__name-dot font-regular">Type</span>
+                    <select className="form__select" defaultValue="password" onChange={(e) => setIsPassword(e.target.value === "password")}>
+                        <option value="password">Password</option>
+                        <option value="document">Document</option>
+                    </select>
+                </div>
+                {isPassword ? (<div className="form__container-div">
+                    <span className="form__name-dot font-regular">Password</span>
+                    <input type="text" className="form__input" onChange={(e) => setPassword(e.target.value)} />
+                </div>) : (<div className="form__container-div">
+                    <span className="form__name-dot font-regular">File(s)</span>
+                    <label htmlFor="input-files" className="label__drag-and-drop">
+                        <input
+                            type="file"
+                            multiple
+                            hidden
+                            id="input-files"
+                            onChange={(e) => {
+                                setFiles(Array.from(e.target.files));
+                                setCountFiles(e.target.files.length);
+                            }}
+                        />
+                        <div className="container__drag-and-drop">
+                            {countFiles === 0 ? (<>
+                                <img src={Cloud} alt="cloud" className="img-cloud" />
+                                <span className="font-regular">Drag and drop or click here <br /> to upload</span>
+                            </>) : (<>
+                                <img src={CloudDone} alt="cloud" className="img-cloud done"/>
+                                <span className="font-regular">Uploaded {countFiles} files</span>
+                            </>)}
+                        </div>
+                    </label>
+                </div>)}
+                <button className="form__button-submit" type="submit" >Submit</button>
             </div>
             {/* <div className='form__add-folder'>
-                            <input type="text" placeholder='name forler' />
-                            <button type="submit">submit</button>
-                        </div> */}
+                <input type="text" placeholder='name forler' />
+                <button type="submit">submit</button>
+            </div> */}
         </form>
     );
 }
